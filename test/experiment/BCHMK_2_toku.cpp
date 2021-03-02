42220ddcefce5c8ec3c631a7a845d19bc737c62f
Evan West <etwest@cs.stonybrook.edu>
2021-03-02 12:29:45 -0500

    small changes/fixes

--- test/experiment/BCHMK_2_toku.cpp
+++ test/experiment/BCHMK_2_toku.cpp
@@ -8,7 +8,7 @@ TEST(Benchmark, BCHMK2Toku) {
   const std::string fname = __FILE__;
   size_t pos = fname.find_last_of("\\/");
   const std::string curr_dir = (std::string::npos == pos) ? "" : fname.substr(0, pos);
-  ifstream in{curr_dir + "/res/1000_0.95_0.5.stream"};
+  ifstream in{curr_dir + "../res/1000_0.95_0.5.stream"};
   Node num_nodes;
   in >> num_nodes;
   long m;
@@ -19,6 +19,6 @@ TEST(Benchmark, BCHMK2Toku) {
     in >> a >> b;
     g.update({{a, b}, INSERT});
   }
-  g.set_cum_in(curr_dir + "/res/1000_0.95_0.5.cum");
+  g.set_cum_in(curr_dir + "../res/1000_0.95_0.5.cum");
   ASSERT_EQ(1000, g.connected_components().size());
 }
