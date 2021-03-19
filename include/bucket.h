f0de88dfc3d14c8b6585870b55316e9c46924c47
kennyzzhang <55612412+kennyzzhang@users.noreply.github.com>
2021-03-19 15:55:24 -0400

    Use XXH32 for 32bit

--- include/bucket.h
+++ include/bucket.h
@@ -14,11 +14,11 @@
  */
 struct Bucket_Boruvka {
   vec_t a = 0;
-  XXH64_hash_t c = 0;
+  vec_t c = 0;
 
-  inline static XXH64_hash_t col_index_hash(const unsigned bucket_col, const vec_t& update_idx, const long sketch_seed);
+  inline static vec_t col_index_hash(const unsigned bucket_col, const vec_t& update_idx, const long sketch_seed);
 
-  inline static XXH64_hash_t index_hash(const vec_t& index, long seed);
+  inline static vec_t index_hash(const vec_t& index, long seed);
 
   /**
    * Checks whether the hash associated with the Bucket hashes the index to 0.
@@ -27,7 +27,7 @@ struct Bucket_Boruvka {
    * @param guess_nonzero
    * @return true if the index is NOT hashed to zero.
    */
-  inline bool contains(const XXH64_hash_t& index_hash, const vec_t& guess_nonzero) const;
+  inline bool contains(const vec_t& index_hash, const vec_t& guess_nonzero) const;
 
   /**
    * Checks whether this Bucket is good.
@@ -45,22 +45,34 @@ struct Bucket_Boruvka {
    * @param update
    * @param bucket_seed This Bucket's seed, generated with gen_bucket_seed.
    */
-  inline void update(const vec_t& update_idx, const XXH64_hash_t& update_hash);
+  inline void update(const vec_t& update_idx, const vec_t& update_hash);
 };
 
-inline XXH64_hash_t Bucket_Boruvka::col_index_hash(const unsigned bucket_col, const vec_t& update_idx, const long sketch_seed) {
+inline vec_t Bucket_Boruvka::col_index_hash(const unsigned bucket_col, const vec_t& update_idx, const long sketch_seed) {
   struct {
     unsigned bucket_col;
     vec_t update_idx;
   } __attribute__((packed)) buf = {bucket_col, update_idx};
-  return XXH64(&buf, sizeof(buf), sketch_seed);
+  return
+#ifdef USE_NATIVE_F
+  XXH32
+#else
+  XXH64
+#endif
+      (&buf, sizeof(buf), sketch_seed);
 }
 
-inline XXH64_hash_t Bucket_Boruvka::index_hash(const vec_t& index, long sketch_seed) {
-  return XXH64(&index, sizeof(index), sketch_seed);
+inline vec_t Bucket_Boruvka::index_hash(const vec_t& index, long sketch_seed) {
+  return
+#ifdef USE_NATIVE_F
+  XXH32
+#else
+  XXH64
+#endif
+      (&index, sizeof(index), sketch_seed);
 }
 
-inline bool Bucket_Boruvka::contains(const XXH64_hash_t& col_index_hash, const vec_t& guess_nonzero) const {
+inline bool Bucket_Boruvka::contains(const vec_t& col_index_hash, const vec_t& guess_nonzero) const {
   return col_index_hash % guess_nonzero == 0;
 }
 
@@ -69,7 +81,7 @@ inline bool Bucket_Boruvka::is_good(const vec_t& n, const unsigned bucket_col, c
     && contains(col_index_hash(bucket_col, a, sketch_seed), guess_nonzero);
 }
 
-inline void Bucket_Boruvka::update(const vec_t& update_idx, const XXH64_hash_t& update_hash) {
+inline void Bucket_Boruvka::update(const vec_t& update_idx, const vec_t& update_hash) {
   a ^= update_idx;
   c ^= update_hash;
 }
