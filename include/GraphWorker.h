6e3385eed221b4922f6d9f04950d8e28796e9961
Evan West <evtwest@yahoo.com>
2021-03-30 11:55:10 -0400

    and a header

---
+++ include/GraphWorker.h
@@ -0,0 +1,39 @@
+#ifndef WORKER_GUARD
+#define WORKER_GUARD
+
+#include <atomic>
+#include <queue>
+#include <pthread.h>
+
+// forward declarations
+class TokuInterface;
+class Graph;
+
+
+class GraphWorker {
+public:
+	GraphWorker(int _id, Graph *_graph, TokuInterface *_db);
+	~GraphWorker();
+
+	static std::atomic_flag queue_lock;
+	static std::queue<uint64_t> work_queue;
+private:
+	static void *startWorker(void *obj) {
+		((GraphWorker *)obj)->doWork();
+		return NULL;
+	}
+
+	void doWork();
+	pthread_t thr;
+	Graph *graph;
+	TokuInterface *db;
+	int id;
+	bool shutdown = false;
+};
+
+// TODO:
+//  * lock the nodes: So that there are not multiple workers updating the
+//    same sketch. Probably very unlikely (with big tau) but still good
+//    to do.
+
+#endif
