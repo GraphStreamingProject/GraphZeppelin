e0df39c48a12e46283aab701250200069fe8fdf8
Evan West <evtwest@yahoo.com>
2021-04-01 14:13:34 -0400

    added insertions progress bar

--- test/experiment/BCHMK_2_toku.cpp
+++ test/experiment/BCHMK_2_toku.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <fstream>
+#include <string>
 #include "../../include/graph.h"
 #include "../util/graph_verifier.h"
 #include "../util/graph_gen.h"
@@ -8,22 +9,31 @@ TEST(Benchmark, BCHMK2Toku) {
   const std::string fname = __FILE__;
   size_t pos = fname.find_last_of("\\/");
   const std::string curr_dir = (std::string::npos == pos) ? "" : fname.substr(0, pos);
-  ifstream in{curr_dir + "/../res/1000_0.95_0.5.stream"};
+  ifstream in{curr_dir + "/../res/3000.test.stream"};
   Node num_nodes;
   in >> num_nodes;
   long m;
   in >> m;
+  long total = m;
   Node a, b;
   uint8_t u;
   Graph g{num_nodes};
+  printf("Insertions\n");
+  printf("Progress:                    | 0%%\r"); fflush(stdout);
   while (m--) {
+    if ((total - m) % (int)(total * .05) == 0) {
+      int percent = (total - m) / (total * .05);
+      printf("Progress:%s%s", std::string(percent, '=').c_str(), std::string(20 - percent, ' ').c_str());
+      printf("| %i%%\r", percent * 5); fflush(stdout);
+    }
     in >> u >> a >> b;
     //printf("a = %lu b = %lu\n", a, b);
     if (u == INSERT)
-	g.update({{a, b}, INSERT});
+      g.update({{a, b}, INSERT});
     else
-	g.update({{a,b}, DELETE});
+      g.update({{a,b}, DELETE});
   }
-  g.set_cum_in(curr_dir + "/../res/1000_0.95_0.5.cum");
+  printf("Progress:====================| Done\n");
+  g.set_cum_in(curr_dir + "/../res/3000.test.cum");
   ASSERT_EQ(1, g.connected_components().size());
 }
