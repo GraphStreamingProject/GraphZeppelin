df7abd9a969882704df09bdb72a2d4854ace8594
kennyzzhang <55612412+kennyzzhang@users.noreply.github.com>
2021-05-27 14:59:02 -0400

    Add DSU header

---
+++ include/dsu.h
@@ -0,0 +1,38 @@
+class DSU {
+  std::vector<Node> parent;
+  std::vector<uint8_t> rank;
+  public:
+  inline DSU(Node n);
+  inline void merge(Node a, Node b);
+  inline void link(Node a, Node b);
+  inline Node find(Node n);
+  inline bool is_rep(Node n);
+};
+
+inline DSU::DSU(Node n) : rank(n) {
+  parent.reserve(n);
+  for (decltype(n) i = 0; i < n; i++) {
+    parent.push_back(i);
+  }
+}
+
+inline void DSU::merge(Node a, Node b) {
+  link(find(a), find(b));
+}
+
+inline void DSU::link(Node a, Node b) {
+  if (a == b) return;
+  if (rank[a] < rank[b]) std::swap(a, b);
+  parent[a] = b;
+  if (rank[a] == rank[b]) rank[a]++;
+}
+
+inline Node DSU::find(Node n) {
+  while (parent[n] != n)
+    n = parent[n] = parent[parent[n]];
+  return n;
+}
+
+bool DSU::is_rep(Node n) {
+  return parent[n] == n;
+}
