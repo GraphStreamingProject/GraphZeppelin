da18098985c079b82dd85e1be89f77def14d75fd
kennyzzhang <55612412+kennyzzhang@users.noreply.github.com>
2021-06-04 15:01:00 -0400

    Initial implementation

--- graph.cpp
+++ graph.cpp
@@ -10,14 +10,10 @@ Graph::Graph(uint64_t num_nodes): num_nodes(num_nodes) {
 #ifdef VERIFY_SAMPLES_F
   cout << "Verifying samples..." << endl;
 #endif
-  representatives = new set<Node>();
   supernodes = new Supernode*[num_nodes];
-  parent = new Node[num_nodes];
   time_t seed = time(nullptr);
   for (Node i=0;i<num_nodes;++i) {
-    representatives->insert(i);
     supernodes[i] = new Supernode(num_nodes,seed);
-    parent[i] = i;
   }
   num_updates = 0; // REMOVE this later
   std::string buffer_loc_prefix = configure_system(); // read the configuration file to configure the system
@@ -38,8 +34,6 @@ Graph::~Graph() {
   for (unsigned i=0;i<num_nodes;++i)
     delete supernodes[i];
   delete[] supernodes;
-  delete[] parent;
-  delete representatives;
   GraphWorker::stop_workers(); // join the worker threads
 #ifdef USE_FBT_F
   delete bf;
@@ -91,11 +85,20 @@ vector<set<Node>> Graph::connected_components() {
   // after this point all updates have been processed from the buffer tree
 
   printf("Total number of updates to sketches before CC %lu\n", num_updates.load()); // REMOVE this later
-  update_locked = true; // disallow updating the graph after we run the alg
+//  update_locked = true; // disallow updating the graph after we run the alg
   bool modified;
 #ifdef VERIFY_SAMPLES_F
   GraphVerifier verifier {cum_in};
 #endif
+
+  representatives = new set<Node>();
+  parent = new Node[num_nodes];
+  Supernode* supernodes[] = new Supernode*[num_nodes];
+  for (Node i=0;i<num_nodes;++i) {
+    supernodes[i] = new Supernode(*this->supernodes[i]);
+    representatives->insert(i);
+    parent[i] = i;
+  }
 
   do {
     modified = false;
@@ -138,6 +141,12 @@ vector<set<Node>> Graph::connected_components() {
 #ifdef VERIFY_SAMPLES_F
   verifier.verify_soln(retval);
 #endif
+
+  for (unsigned i=0;i<num_nodes;++i)
+    delete supernodes[i];
+  delete[] supernodes;
+  delete[] parent;
+  delete representatives;
 
   return retval;
 }
---
+++ test/continuous_correctness_test.cpp
@@ -0,0 +1,60 @@
+#include <gtest/gtest.h>
+#include <fstream>
+#include <unordered_map>
+#include "../include/graph.h"
+#include "util/graph_gen.h"
+
+void test_continuous(unsigned nodes, unsigned long updates_per_sample, unsigned long samples) {
+  srand(time(NULL));
+  Graph g(nodes);
+  size_t total_edges = static_cast<size_t>(nodes - 1) * nodes / 2;
+  std::vector<bool> adj(total_edges);
+  unsigned long num_failure = 0;
+  for (unsigned long i = 0; i < samples; i++) {
+    for (unsigned long j = 0; j < updates_per_sample; j++) {
+      unsigned edgei = rand() % nodes;
+      unsigned edgej = rand() % (nodes - 1);
+      if (edgei > edgej) {
+        std::swap(edgei, edgej);
+      } else {
+        edgej++;
+      }
+      uint64_t edgeidx = nondirectional_non_self_edge_pairing_fn(edgei, edgej);
+      g.update({{edgei, edgej}, INSERT});
+      adj[edgeidx] = !adj[edgeidx];
+    }
+    try {
+      vec_t num_edges = 0;
+      for (uint64_t i = 0; i < total_edges; i++) {
+        if (adj[i]) {
+          num_edges++;
+        }
+      }
+      std::ofstream out("./cum_sample.txt");
+      out << nodes << " " << num_edges << std::endl;
+      for (unsigned i = 0; i < total_edges; i++) {
+        if (adj[i]) {
+          auto edge = inv_nondir_non_self_edge_pairing_fn(i);
+          out << edge.first << " " << edge.second << std::endl;
+        }
+      }
+      out.close();
+      g.set_cum_in("./cum_sample.txt");
+      g.connected_components();
+    } catch (const NoGoodBucketException& e) {
+      num_failure++;
+    } catch (const NotCCException& e) {
+      num_failure++;
+    } catch (const BadEdgeException& e) {
+      num_failure++;
+    }
+  }
+  std::clog << nodes << ',' << num_failure << std::endl;
+}
+
+TEST(GraphTestSuite, DISABLED_TestContinuous) {
+  for (int i = 10, n = i; i < 1e4; n = n > i ? (i *= 10) : n * sqrt(10)) {
+    if (n == 10) continue; // Supernode cannot be sampled more times, probably bug
+    test_continuous(n, 1000, 1000);
+  }
+}
