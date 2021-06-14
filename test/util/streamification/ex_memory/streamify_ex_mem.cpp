6c8ffc68304ae432171db7e17b67599a38a796ce
JAD <jad552255@gmail.com>
2021-06-14 16:38:58 +0000

    Created in-memory version of streamification without update types

---
+++ test/util/streamification/ex_memory/streamify_ex_mem.cpp
@@ -0,0 +1,160 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <chrono>
+#include <unistd.h>
+#include <cmath>
+
+using namespace std;
+
+int main (int argc, char * argv [])
+{
+	int notification_frequency = 5000000;
+
+	string static_graph_file_name = argv[1];
+	double static_reinsertion_param = atof(argv[2]);
+	unsigned long static_reinsertion_cap;
+	sscanf(argv[3], "%lu", &static_reinsertion_cap);
+	double general_insertion_param = atof(argv[4]);
+	unsigned long general_insertion_cap;
+	sscanf(argv[5], "%lu", &general_insertion_cap);
+	unsigned long num_general_inserts;
+	sscanf(argv[6], "%lu", &num_general_inserts);
+	string stream_file_name = argv[7];
+
+	ifstream static_graph_file{static_graph_file_name};
+	unsigned long num_nodes, num_edges;
+	static_graph_file >> num_nodes >> num_edges;
+
+	ofstream stream_file_out{stream_file_name}; 
+
+	unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+	default_random_engine generator(seed);
+	uniform_int_distribution<int> rand_char(33, 126);
+
+	int rand_prefix_len = log((double) num_edges * (2 * 1 / static_reinsertion_param + 1) 
+			+ num_general_inserts * (2 * 1 / general_insertion_param)) / log(10.0);
+
+	unsigned long total_num_updates = 0;	
+	char * random_prefix = new char [rand_prefix_len];
+
+	// Static graph geometric inserts/deletes	
+	
+	cout << "Constructing updates for edges in input graph..." << endl;
+
+	geometric_distribution<unsigned long> static_reinsertions(static_reinsertion_param);
+	unsigned long u, v;
+	unsigned long num_reinserts, num_updates;
+	for (unsigned long i = 0; i < num_edges; i++)
+	{
+		if (i % notification_frequency == 0 && i != 0)
+			cout << i << " edges completed..." << endl; 
+
+		static_graph_file >> u >> v;
+		num_reinserts = min(static_reinsertion_cap, static_reinsertions(generator));
+		// Want edges to be contained in final state of graph
+		num_updates = 2 * num_reinserts + 1;
+
+		for (unsigned long j = 0; j < num_updates; j++)
+		{	
+			for (int k = 0; k < rand_prefix_len; k++)
+				random_prefix[k] = (char) rand_char(generator);
+		
+			stream_file_out << random_prefix << "\t" << u << "\t" << v << "\n";
+		}
+
+		total_num_updates += num_updates;
+	}	
+
+	// General geometric inserts/deletes	
+	
+	cout << "Constructing updates for random possible edges..." << endl;
+
+	geometric_distribution<unsigned long> general_insertions(general_insertion_param);
+	uniform_int_distribution<unsigned long> random_node(0, num_nodes - 1);
+	unsigned long num_inserts;
+	for (unsigned long i = 0; i < num_general_inserts; i++)
+	{
+		if (i % notification_frequency == 0 && i != 0)
+			cout << i << " edges completed..." << endl; 
+
+		unsigned long rand_u = random_node(generator);
+		unsigned long rand_v = random_node(generator);
+		
+		num_inserts = min(general_insertion_cap, general_insertions(generator));
+		// These edges do not persist to the end of the stream
+		num_updates = 2 * num_inserts;
+
+		for (unsigned long j = 0; j < num_updates; j++)
+		{	
+			for (int k = 0; k < rand_prefix_len; k++)
+				random_prefix[k] = (char) rand_char(generator);
+		
+			stream_file_out << random_prefix << "\t" << rand_u << "\t" << rand_v << "\n";
+		}
+
+		total_num_updates += num_updates;
+	}
+
+	delete [] random_prefix;
+	stream_file_out.flush();
+
+	// External memory sort by the random prefix to produce 
+	// a random permutation
+
+	cout << "Conducting random external memory permutation of updates..." << endl;
+	system((string("sort -k 1,1 ") + stream_file_name + string(" -o ") + stream_file_name).c_str());
+
+	// Remove random prefixes used for permuting
+	// Insert prefix denoting insertion or deletion
+	
+	cout << "Pruning temporary prefixes and adding update types..." << endl;
+
+	vector<bool> edge_present(num_nodes * (num_nodes - 1), false);
+
+	stream_file_out.clear();
+	stream_file_out.seekp(0);
+	// Mantain separate streams to avoid unnecessary flushes
+	ifstream stream_file_in{stream_file_name};
+
+	string prefix;
+	unsigned long index;
+	char upd_type;
+
+	unsigned long temp_u, temp_v;
+	stream_file_in >> prefix >> temp_u >> temp_v;
+	stream_file_out << num_nodes << '\t' << total_num_updates << '\n';
+
+	for (unsigned long i = 0; i < total_num_updates - 1; i++)
+	{
+		if (i % notification_frequency == 0 && i != 0)
+			cout << i << " updates completed..." << endl;
+
+		stream_file_in >> prefix >> u >> v;
+		
+		index = u * num_nodes + v;
+		upd_type = edge_present[index] ? '1' : '0';
+		edge_present[index] = !edge_present[index];
+
+		stream_file_out << upd_type << '\t' << u << '\t' << v << '\n';
+	}
+
+	// Reinsert femporarily removed first update
+	index = temp_u * num_nodes + temp_v;
+	upd_type = edge_present[index] ? '1' : '0';
+	edge_present[index] = !edge_present[index];
+
+	stream_file_out << upd_type << '\t' << temp_u << '\t' << temp_v << '\n';
+
+	// Truncate remainder of file
+	
+	if (-1 == truncate(stream_file_name.c_str(), stream_file_out.tellp()))
+	{
+		cout << "Truncation error" << endl;
+		return 1;
+	}
+
+	return 0;
+}
---
+++ test/util/streamification/in_memory/streamify_in_mem.cpp
@@ -0,0 +1,110 @@
+#include <stdlib.h>
+#include <utility>
+#include <algorithm>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <chrono>
+#include <unistd.h>
+#include <cmath>
+
+using namespace std;
+
+// NOTE: Only assumesup to 2^32 nodes in graph
+
+int main (int argc, char * argv [])
+{
+	int notification_frequency = 5000000;
+
+	string static_graph_file_name = argv[1];
+	double static_reinsertion_param = atof(argv[2]);
+	unsigned long static_reinsertion_cap;
+	sscanf(argv[3], "%lu", &static_reinsertion_cap);
+	double general_insertion_param = atof(argv[4]);
+	unsigned long general_insertion_cap;
+	sscanf(argv[5], "%lu", &general_insertion_cap);
+	unsigned long num_general_inserts;
+	sscanf(argv[6], "%lu", &num_general_inserts);
+	string stream_file_name = argv[7];
+
+	ifstream static_graph_file{static_graph_file_name};
+	unsigned int num_nodes;
+        unsigned long num_edges;
+	static_graph_file >> num_nodes >> num_edges;
+
+
+	typedef std::pair<unsigned int, unsigned int> edge;
+	vector<edge> updates;
+	// TODO: reserve some appropriate space to minimize resizing
+
+	unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+	default_random_engine generator(seed);
+
+	unsigned long total_num_updates = 0;	
+
+	// Static graph geometric inserts/deletes	
+	
+	cout << "Constructing updates for edges in input graph..." << endl;
+
+	geometric_distribution<unsigned long> static_reinsertions(static_reinsertion_param);
+	unsigned long u, v;
+	unsigned long num_reinserts, num_updates;
+	for (unsigned long i = 0; i < num_edges; i++)
+	{
+		if (i % notification_frequency == 0 && i != 0)
+			cout << i << " edges completed..." << endl; 
+
+		static_graph_file >> u >> v;
+		num_reinserts = min(static_reinsertion_cap, static_reinsertions(generator));
+		// Want edges to be contained in final state of graph
+		num_updates = 2 * num_reinserts + 1;
+
+		for (unsigned long j = 0; j < num_updates; j++)
+			updates.push_back(std::make_pair(u, v));
+
+		total_num_updates += num_updates;
+	}	
+
+	// General geometric inserts/deletes	
+	
+	cout << "Constructing updates for random possible edges..." << endl;
+
+	geometric_distribution<unsigned long> general_insertions(general_insertion_param);
+	uniform_int_distribution<unsigned long> random_node(0, num_nodes - 1);
+	unsigned long num_inserts;
+	for (unsigned long i = 0; i < num_general_inserts; i++)
+	{
+		if (i % notification_frequency == 0 && i != 0)
+			cout << i << " edges completed..." << endl; 
+
+		unsigned long rand_u = random_node(generator);
+		unsigned long rand_v = random_node(generator);
+		
+		num_inserts = min(general_insertion_cap, general_insertions(generator));
+		// These edges do not persist to the end of the stream
+		num_updates = 2 * num_inserts;
+
+		for (unsigned long j = 0; j < num_updates; j++)
+			updates.push_back(std::make_pair(rand_u, rand_v));
+
+		total_num_updates += num_updates;
+	}
+
+	
+	// In-memory shuffle of updates
+	
+	cout << "Conducting in-memory shuffle of generated updates..." << endl;
+	std::shuffle(updates.begin(), updates.end(), generator);
+
+	// Output updates to stream file	
+	
+	cout << "Writing stream to file..." << endl;
+	ofstream stream_file_out{stream_file_name}; 
+	stream_file_out << num_nodes << '\t' << total_num_updates << '\n';
+
+	for (unsigned long i = 0; i < total_num_updates; i++)
+		stream_file_out << updates[i].first << '\t' << updates[i].second << '\n';	
+
+	return 0;
+}
