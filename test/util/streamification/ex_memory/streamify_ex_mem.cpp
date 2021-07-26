5a8eb8cbb29a4558f83f7baa1b811bdc299d4ed8
Evan West <etwest@cs.stonybrook.edu>
2021-07-26 11:24:45 -0400

    updates to external streamer

--- test/util/streamification/ex_memory/streamify_ex_mem.cpp
+++ test/util/streamification/ex_memory/streamify_ex_mem.cpp
@@ -11,7 +11,12 @@ using namespace std;
 
 int main (int argc, char * argv [])
 {
-	int notification_frequency = 5000000;
+	int notification_frequency = 500000000;
+
+	if (argc != 9) {
+		printf("Incorrect number of arguments! Expected 8. See README\n");
+		exit(1);
+	}
 
 	string static_graph_file_name = argv[1];
 	double static_reinsertion_param = atof(argv[2]);
@@ -20,14 +25,19 @@ int main (int argc, char * argv [])
 	double general_insertion_param = atof(argv[4]);
 	unsigned long general_insertion_cap;
 	sscanf(argv[5], "%lu", &general_insertion_cap);
-	unsigned long num_general_inserts;
-	sscanf(argv[6], "%lu", &num_general_inserts);
-	string stream_file_name = argv[7];
-
+	float general_inserts_factor;
+	sscanf(argv[6], "%f", &general_inserts_factor);
+	unsigned int num_iso_nodes;
+	sscanf(argv[7], "%u", &num_iso_nodes);
+	string stream_file_name = argv[8];
+	
 	ifstream static_graph_file{static_graph_file_name};
 	unsigned long num_nodes, num_edges;
 	static_graph_file >> num_nodes >> num_edges;
 
+	unsigned long num_general_inserts = num_edges * general_inserts_factor;
+	std::cout << "num_nodes = " << num_nodes << ", stream_size = " << num_general_inserts << "\n";
+	
 	ofstream stream_file_out{stream_file_name}; 
 
 	unsigned seed = chrono::system_clock::now().time_since_epoch().count();
@@ -53,9 +63,16 @@ int main (int argc, char * argv [])
 			cout << i << " edges completed..." << endl; 
 
 		static_graph_file >> u >> v;
-		num_reinserts = min(static_reinsertion_cap, static_reinsertions(generator));
-		// Want edges to be contained in final state of graph
+		
 		num_updates = 2 * num_reinserts + 1;
+		num_reinserts = min(static_reinsertion_cap, static_reinsertions(generator));
+		if (u > num_nodes - num_iso_nodes - 1 || v > num_nodes - num_iso_nodes - 1) {
+			// Isolate the last num_iso_nodes nodes so insert multiple of 2 times
+			num_updates = 2 * num_reinserts + 2;
+		} else {
+			// Want edges to be contained in final state of graph
+			num_updates = 2 * num_reinserts + 1;
+		}
 
 		for (unsigned long j = 0; j < num_updates; j++)
 		{	
@@ -105,7 +122,7 @@ int main (int argc, char * argv [])
 	// a random permutation
 
 	cout << "Conducting random external memory permutation of updates..." << endl;
-	system((string("sort -k 1,1 ") + stream_file_name + string(" -o ") + stream_file_name).c_str());
+	system((string("sort -k 1,1 -S 75%% --parallel=46 -t\'	\' -s -T /mnt/nvme/tmp ") + stream_file_name + string(" -o ") + stream_file_name).c_str());
 
 	// Remove random prefixes used for permuting
 	// Insert prefix denoting insertion or deletion
