a50be4063726cb081af93a64c8b17bb25a755679
kennyzzhang <55612412+kennyzzhang@users.noreply.github.com>
2021-07-29 10:53:01 -0400

    Collapse identical first buckets

--- l0_sampling/sketch.cpp
+++ l0_sampling/sketch.cpp
@@ -5,8 +5,8 @@ Sketch::Sketch(vec_t n, long seed, double num_bucket_factor):
     seed(seed), n(n), num_bucket_factor(num_bucket_factor) {
   const unsigned num_buckets = bucket_gen(n, num_bucket_factor);
   const unsigned num_guesses = guess_gen(n);
-  bucket_a = std::vector<vec_t>(num_buckets * num_guesses);
-  bucket_c = std::vector<vec_hash_t>(num_buckets * num_guesses);
+  bucket_a = std::vector<vec_t>(num_buckets * num_guesses + 1);
+  bucket_c = std::vector<vec_hash_t>(num_buckets * num_guesses + 1);
 }
 
 Sketch::Sketch(const Sketch &old) : seed(old.seed), n(old.n),
@@ -19,11 +19,12 @@ void Sketch::update(const vec_t& update_idx) {
   const unsigned num_buckets = bucket_gen(n, num_bucket_factor);
   const unsigned num_guesses = guess_gen(n);
   XXH64_hash_t update_hash = Bucket_Boruvka::index_hash(update_idx, seed);
+  Bucket_Boruvka::update(bucket_a[num_buckets * num_guesses], bucket_c[num_buckets * num_guesses], update_idx, update_hash);
   for (unsigned i = 0; i < num_buckets; ++i) {
     col_hash_t col_index_hash = Bucket_Boruvka::col_index_hash(i, update_idx, seed);
     for (unsigned j = 0; j < num_guesses; ++j) {
       unsigned bucket_id = i * num_guesses + j;
-      if (Bucket_Boruvka::contains(col_index_hash, 1 << j)){
+      if (Bucket_Boruvka::contains(col_index_hash, 2 << j)){
         Bucket_Boruvka::update(bucket_a[bucket_id], bucket_c[bucket_id], update_idx, update_hash);
       } else break;
     }
@@ -44,13 +45,20 @@ vec_t Sketch::query() {
   bool all_buckets_zero = true;
   const unsigned num_buckets = bucket_gen(n, num_bucket_factor);
   const unsigned num_guesses = guess_gen(n);
+  if (bucket_a[num_buckets * num_guesses] != 0 || bucket_c[num_buckets * num_guesses] != 0) {
+    all_buckets_zero = false;
+  }
+  if (Bucket_Boruvka::is_good(bucket_a[num_buckets * num_guesses], bucket_c[num_buckets * num_guesses], n, seed)) {
+    return bucket_a[num_buckets * num_guesses];
+  }
+
   for (unsigned i = 0; i < num_buckets; ++i) {
     for (unsigned j = 0; j < num_guesses; ++j) {
       unsigned bucket_id = i * num_guesses + j;
-      if (bucket_a[bucket_id] != 0 || bucket_c[bucket_id] != 0) {
+      if (all_buckets_zero && (bucket_a[bucket_id] != 0 || bucket_c[bucket_id] != 0)) {
         all_buckets_zero = false;
       }
-      if (Bucket_Boruvka::is_good(bucket_a[bucket_id], bucket_c[bucket_id], n, i, 1 << j, seed)) {
+      if (Bucket_Boruvka::is_good(bucket_a[bucket_id], bucket_c[bucket_id], n, i, 2 << j, seed)) {
         return bucket_a[bucket_id];
       }
     }
@@ -85,16 +93,24 @@ bool operator== (const Sketch &sketch1, const Sketch &sketch2) {
 std::ostream& operator<< (std::ostream &os, const Sketch &sketch) {
   const unsigned long long int num_buckets = bucket_gen(sketch.n, sketch.num_bucket_factor);
   const unsigned long long int num_guesses = guess_gen(sketch.n);
+  for (unsigned k = 0; k < sketch.n; k++) {
+    os << '1';
+  }
+  os << std::endl
+     << "a:" << sketch.bucket_a[num_buckets * num_guesses] << std::endl
+     << "c:" << sketch.bucket_c[num_buckets * num_guesses] << std::endl
+     << (Bucket_Boruvka::is_good(sketch.bucket_a[num_buckets * num_guesses], sketch.bucket_c[num_buckets * num_guesses], sketch.n, sketch.seed) ? "good" : "bad") << std::endl;
+
   for (unsigned i = 0; i < num_buckets; ++i) {
     for (unsigned j = 0; j < num_guesses; ++j) {
       unsigned bucket_id = i * num_guesses + j;
       for (unsigned k = 0; k < sketch.n; k++) {
-        os << (Bucket_Boruvka::contains(Bucket_Boruvka::col_index_hash(i, k, sketch.seed), 1 << j) ? '1' : '0');
+        os << (Bucket_Boruvka::contains(Bucket_Boruvka::col_index_hash(i, k, sketch.seed), 2 << j) ? '1' : '0');
       }
       os << std::endl
          << "a:" << sketch.bucket_a[bucket_id] << std::endl
          << "c:" << sketch.bucket_c[bucket_id] << std::endl
-         << (Bucket_Boruvka::is_good(sketch.bucket_a[bucket_id], sketch.bucket_c[bucket_id], sketch.n, i, 1 << j, sketch.seed) ? "good" : "bad") << std::endl;
+         << (Bucket_Boruvka::is_good(sketch.bucket_a[bucket_id], sketch.bucket_c[bucket_id], sketch.n, i, 2 << j, sketch.seed) ? "good" : "bad") << std::endl;
     }
   }
   return os;
