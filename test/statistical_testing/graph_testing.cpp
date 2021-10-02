c59312d9f218a7b7db0b730196b054ad8e060db8
Evan West <etwest@cs.stonybrook.edu>
2021-10-02 11:20:56 -0400

    Real expected statistics and small script improvements

--- test/statistical_testing/graph_testing.cpp
+++ test/statistical_testing/graph_testing.cpp
@@ -1,8 +1,8 @@
 #include <fstream>
 #include <iostream>
-#include "../include/graph.h"
-#include "../test/util/graph_gen.h"
-#include "../test/util/graph_verifier.h"
+#include "../../include/graph.h"
+#include "../util/graph_gen.h"
+#include "../util/graph_verifier.h"
 
 static inline int do_run() {
     ifstream in{"./sample.txt"};
@@ -25,19 +25,10 @@ static inline int do_run() {
     return 0;
 }
 
-int tiny_graph_test(int runs) {
-    int failures = 0;
-    for (int i = 0; i < runs; i++) {
-        generate_stream({1024,0.001,0.5,0,"./sample.txt","./cumul_sample.txt"});
-        failures += do_run();
-    }
-    return failures;
-}
-
 int small_graph_test(int runs) {
     int failures = 0;
+    generate_stream({1024,0.002,0.5,0,"./sample.txt","./cumul_sample.txt"});
     for (int i = 0; i < runs; i++) {
-        generate_stream({1024,0.03,0.5,0,"./sample.txt","./cumul_sample.txt"});
         failures += do_run();
     }
     return failures;
@@ -45,21 +36,21 @@ int small_graph_test(int runs) {
 
 int medium_graph_test(int runs) {
     int failures = 0;
+    generate_stream({2048,0.002,0.5,0,"./sample.txt","./cumul_sample.txt"});
     for (int i = 0; i < runs; i++) {
-        generate_stream({1024,0.20,0.5,0,"./sample.txt","./cumul_sample.txt"});
         failures += do_run();
     }
     return failures;
 }
 
-int large_with_iso_test(int runs) {
+int medium_with_iso_test(int runs) {
     int failures = 0;
+    generate_stream({2048,0.002,0.5,0,"./sample.txt","./cumul_sample.txt"});
+    std::fstream graph_file{"./sample.txt"};
+    std::fstream cumul_file{"./cumul_sample.txt"};
+    graph_file.write("2070", 4); // increase the node size to create iso nodes
+    cumul_file.write("2070", 4); // do the same for the cumulative file
     for (int i = 0; i < runs; i++) {
-        generate_stream({1024,0.5,0.5,0,"./sample.txt","./cumul_sample.txt"});
-        std::fstream graph_file{"./sample.txt"};
-        std::fstream cumul_file{"./cumul_sample.txt"};
-        graph_file.write("1074", 4); // increase the node size to create iso nodes
-        cumul_file.write("1074", 4); // do the same for the cumulative file
         failures += do_run();
     }
     return failures;
@@ -67,26 +58,15 @@ int large_with_iso_test(int runs) {
 
 int main() {
     int runs = 10;
-    int num_trails = 2;
+    int num_trails = 5000;
     std::vector<int> trial_list;
     std::ofstream out;
     
-    /************* tiny graph test **************/
-    out.open("./tiny_graph_test");
-    for(int i = 0; i < num_trails; i++) {
-        int trial_result = tiny_graph_test(runs);
-        trial_list.push_back(trial_result);
-    }
-    // output the results of these trials
-    for (unsigned i = 0; i < trial_list.size(); i++) {
-        out << trial_list[i] << " " << runs << "\n";
-    }
-    trial_list.clear();
-    out.close();
-
-    /************* small graph test *************/
+    /************* small graph test **************/
+    fprintf(stderr, "small graph test\n");
     out.open("./small_graph_test");
     for(int i = 0; i < num_trails; i++) {
+        if (i % 100 == 0) fprintf(stderr, "trial %i\n", i);
         int trial_result = small_graph_test(runs);
         trial_list.push_back(trial_result);
     }
@@ -98,8 +78,10 @@ int main() {
     out.close();
 
     /************* medium graph test ************/
+    fprintf(stderr, "medium graph test\n");
     out.open("./medium_graph_test");
     for(int i = 0; i < num_trails; i++) {
+        if (i % 100 == 0) fprintf(stderr, "trial %i\n", i);
         int trial_result = medium_graph_test(runs);
         trial_list.push_back(trial_result);
     }
@@ -111,9 +93,11 @@ int main() {
     out.close();
 
     /************** large iso test **************/
-    out.open("./large_with_iso_test");
+    fprintf(stderr, "medium_iso graph test\n");
+    out.open("./medium_with_iso_test");
     for(int i = 0; i < num_trails; i++) {
-        int trial_result = large_with_iso_test(runs);
+        if (i % 100 == 0) fprintf(stderr, "trial %i\n", i);
+        int trial_result = medium_with_iso_test(runs);
         trial_list.push_back(trial_result);
     }
     // output the results of these trials
