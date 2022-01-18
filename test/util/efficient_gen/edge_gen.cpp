d2451257c20468b15b17c9ab5394458103322199
Evan West <evtwest@yahoo.com>
2022-01-18 16:31:38 -0500

    fixed efficient gen

--- test/util/efficient_gen/edge_gen.cpp
+++ test/util/efficient_gen/edge_gen.cpp
@@ -6,8 +6,6 @@
 #include "../../../include/test/efficient_gen.h"
 #include "../../../include/types.h"
 
-#define endl '\n'
-
 typedef uint32_t ul;
 typedef uint64_t ull;
 
@@ -33,19 +31,27 @@ std::ofstream& operator<< (std::ofstream &os, const std::pair<ull,ull> p) {
   return os;
 }
 
-void write_edges(int n, double p, std::string out_f) {
-  ul num_edges = (n*(n-1))/2;
-  ul* arr = (ul*) malloc(num_edges*sizeof(ul));
-  for (unsigned i=0;i<num_edges;++i) {
-    arr[i] = i;
+void write_edges(ul n, double p, std::string out_f) {
+  ull num_edges = (n*(n-1))/2;
+  ull* arr = (ull*) malloc(num_edges*sizeof(ull));
+  ul idx = 0;
+
+  std::cout << "Generating possible edges" << std::endl;
+  for (unsigned i=0; i < n; ++i) {
+    for (unsigned j=i+1;j < n; ++j) {
+      arr[idx++] = nondirectional_non_self_edge_pairing_fn(i, j);
+    }
   }
+
+  std::cout << "Permuting edges" << std::endl;  
   std::random_shuffle(arr,arr+num_edges);
   std::ofstream out(out_f);
-  ul m = (ul) (num_edges*p);
-  out << n << " " << m << endl;
+  ull m = (ull) (num_edges*p);
+  out << n << " " << m << std::endl;
 
+  std::cout << "Writing edges to file" << std::endl;
   while (m--) {
-    out << inv_nondir_non_self_edge_pairing_fn(arr[m]) << endl;
+    out << inv_nondir_non_self_edge_pairing_fn(arr[m]) << std::endl;
   }
 
   out.close();
@@ -53,39 +59,41 @@ void write_edges(int n, double p, std::string out_f) {
 }
 
 void insert_delete(double p, std::string in_file, std::string out_file) {
+  std::cout << "Deleting and reinserting some edges" << std::endl;
   std::ifstream in(in_file);
   std::ofstream out(out_file);
-  int n; ul m; in >> n >> m;
-  long long full_m = m;
-  ul ins_del_arr[(ul)log2(m)+2];
-  std::fill(ins_del_arr,ins_del_arr + (ul)log2(m)+2,0);
+  int n; ull m; in >> n >> m;
+ 
+  ull full_m = m;
+  ull ins_del_arr[(ull)log2(m)+2];
+  std::fill(ins_del_arr,ins_del_arr + (ull)log2(m)+2,0);
   ins_del_arr[0] = m;
   for (unsigned i = 0; ins_del_arr[i] > 1; ++i) {
     ins_del_arr[i+1] = (ul)(ins_del_arr[i]*p);
     full_m += ins_del_arr[i+1];
   }
   
-  out << n << " " << full_m << endl;
+  out << n << " " << full_m << std::endl;
   
-  ul* memoized = (ul*) malloc(ins_del_arr[1]*sizeof(ul));
+  ull* memoized = (ull*) malloc(ins_del_arr[1]*sizeof(ull));
   ul a,b;
   
   for (unsigned i=0;i<ins_del_arr[1];++i) {
     in >> a >> b;
-    out << "0 " << a << " " << b << endl;
+    out << "0 " << a << " " << b << std::endl;
     memoized[i] = nondirectional_non_self_edge_pairing_fn(a, b);
   }
 
   for (unsigned i=ins_del_arr[1];i<m;++i) {
     in >> a >> b;
-    out << "0 " << a << " " << b << endl;
+    out << "0 " << a << " " << b << std::endl;
   }
 
   for (unsigned i = 1; ins_del_arr[i] >= 1; ++i) {
     int temp = i%2;
     for (unsigned j=0;j<ins_del_arr[i];++j) {
       out << temp << " ";
-      out << inv_nondir_non_self_edge_pairing_fn(memoized[j]) << endl;
+      out << inv_nondir_non_self_edge_pairing_fn(memoized[j]) << std::endl;
     }
   }
   free(memoized);
@@ -114,10 +122,10 @@ void write_cumul(const std::string& stream_f, const std::string& cumul_f) {
       if (adj[i][j]) ++m_cumul;
     }
   }
-  out << n << " " << m_cumul << endl;
+  out << n << " " << m_cumul << std::endl;
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < n; ++j) {
-      if (adj[i][j]) out << i << " " << j << endl;
+      if (adj[i][j]) out << i << " " << j << std::endl;
     }
   }
 }
