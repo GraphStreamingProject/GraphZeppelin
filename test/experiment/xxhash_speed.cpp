cab3ea33f2f6e3a21358b18deca7ede25bf53cd3
Evan West <etwest@cs.stonybrook.edu>
2022-02-10 12:47:52 -0500

    xxhash_bench updates

--- test/experiment/xxhash_speed.cpp
+++ test/experiment/xxhash_speed.cpp
@@ -4,6 +4,9 @@
 #include <chrono>
 #include <random>
 
+// Uncomment this to check the quality of the generated hash
+// #define CHECK_QUALITY
+
 typedef uint32_t vec_hash_t;
 typedef uint64_t vec_t;
 static const auto& hash32_func  = XXH32;
@@ -34,28 +37,32 @@ inline std::vector<vec_hash_t> get_hash64_vector(const vec_t& index, long seed, 
   return ret;
 }
 
-void run_32bit_direct_test(uint64_t num_items, uint64_t col_width, int num_trials) {
+void run_32bit_direct_test(uint64_t num_items, uint64_t num_hashes, int num_trials) {
   std::vector<long> zeros(32);
   long seed = 0xBAAABAAA;
   std::mt19937_64 rand(seed);
+  vec_hash_t hash_xor = 0;
 
   auto start = std::chrono::steady_clock::now();
   for (int t = 0; t < num_trials; t++) {
     for (uint64_t i = 0; i < num_items; i++) {
       vec_t index = rand();
-      for (uint64_t c = 0; c < col_width; c++) {
+      for (uint64_t c = 0; c < num_hashes; c++) {
         vec_hash_t hash = get_hash32_direct(index, seed + c);
-      
+	hash_xor ^= hash;
+
+#ifdef CHECK_QUALITY	
         int j = 1;
         while(hash % (1 << j) == 0 && j <= 32) j++;
-
         zeros[j-1]++;
+#endif
       }
     }
   }
   auto end = std::chrono::steady_clock::now();
   long double time_taken = static_cast<std::chrono::duration<long double>>(end - start).count();
   std::cout << "======  32 bit hash direct ======" << std::endl;
+  std::cout << "hash xor " << hash_xor << std::endl;
 
   long sum = 0;
   for (int i = 31; i >= 0; i--) {
@@ -64,36 +71,43 @@ void run_32bit_direct_test(uint64_t num_items, uint64_t col_width, int num_trial
   }
 
   std::cout << "Average time: " << time_taken / num_trials << std::endl;
+  std::cout << "Hash calls per second: " << num_items * num_hashes * num_trials / time_taken << std::endl;
 
+#ifdef CHECK_QUALITY
   std::cout << "Col depth: ";
   for (int i = 0; i < 32; i++) {
-    std::cout << (double)(zeros[i]) / (double)(num_items * col_width * num_trials) << ", ";
+    std::cout << (double)(zeros[i]) / (double)(num_items * num_hashes * num_trials) << ", ";
   }
   std::cout << std::endl;
+#endif
 }
 
-void run_64bit_direct_test(uint64_t num_items, uint64_t col_width, int num_trials) {
+void run_64bit_direct_test(uint64_t num_items, uint64_t num_hashes, int num_trials) {
   std::vector<long> zeros(32);
   long seed = 0xBAAABAAA;
   std::mt19937_64 rand(seed);
+  vec_hash_t hash_xor = 0;
 
   auto start = std::chrono::steady_clock::now();
   for (int t = 0; t < num_trials; t++) {
     for (uint64_t i = 0; i < num_items; i++) {
       vec_t index = rand();
-      for (uint64_t c = 0; c < col_width; c++) {
+      for (uint64_t c = 0; c < num_hashes; c++) {
         vec_hash_t hash = get_hash64_direct(index, seed + c);
+	hash_xor ^= hash;
       
+#ifdef CHECK_QUALITY	
         int j = 1;
         while(hash % (1 << j) == 0 && j <= 32) j++;
-
         zeros[j-1]++;
+#endif
       }
     }
   }
   auto end = std::chrono::steady_clock::now();
   long double time_taken = static_cast<std::chrono::duration<long double>>(end - start).count();
   std::cout << "======  64 bit hash direct ======" << std::endl;
+  std::cout << "hash xor " << hash_xor << std::endl;
 
   long sum = 0;
   for (int i = 31; i >= 0; i--) {
@@ -102,29 +116,35 @@ void run_64bit_direct_test(uint64_t num_items, uint64_t col_width, int num_trial
   }
 
   std::cout << "Average time: " << time_taken / num_trials << std::endl;
+  std::cout << "Hash calls per second: " << num_items * num_hashes * num_trials / time_taken << std::endl;
 
+#ifdef CHECK_QUALITY
   std::cout << "Col depth: ";
   for (int i = 0; i < 32; i++) {
-    std::cout << (double)(zeros[i]) / (double)(num_items * col_width * num_trials) << ", ";
+    std::cout << (double)(zeros[i]) / (double)(num_items * num_hashes * num_trials) << ", ";
   }
   std::cout << std::endl;
+#endif
 }
 
-void run_32bit_vector_test(uint64_t num_items, uint64_t col_width, int num_trials) {
+void run_32bit_vector_test(uint64_t num_items, uint64_t num_hashes, int num_trials) {
   std::vector<long> zeros(32);
   long seed = 0xBAAABAAA;
   std::mt19937_64 rand(seed);
+  vec_hash_t hash_xor = 0;
 
   auto start = std::chrono::steady_clock::now();
   for (int t = 0; t < num_trials; t++) {
     for (uint64_t i = 0; i < num_items; i++) {
       vec_t index = rand();
-      std::vector<vec_hash_t> hashes = get_hash32_vector(index, seed, col_width);
+      std::vector<vec_hash_t> hashes = get_hash32_vector(index, seed, num_hashes);
       for (vec_hash_t hash : hashes) {
+	hash_xor ^= hash;
+#ifdef CHECK_QUALITY	
         int j = 1;
         while(hash % (1 << j) == 0 && j <= 32) j++;
-
         zeros[j-1]++;
+#endif
       }
     }
     
@@ -132,6 +152,7 @@ void run_32bit_vector_test(uint64_t num_items, uint64_t col_width, int num_trial
   auto end = std::chrono::steady_clock::now();
   long double time_taken = static_cast<std::chrono::duration<long double>>(end - start).count();
   std::cout << "======  32 bit hash vector ======" << std::endl;
+  std::cout << "hash xor " << hash_xor << std::endl;
 
   long sum = 0;
   for (int i = 31; i >= 0; i--) {
@@ -140,29 +161,35 @@ void run_32bit_vector_test(uint64_t num_items, uint64_t col_width, int num_trial
   }
 
   std::cout << "Average time: " << time_taken / num_trials << std::endl;
+  std::cout << "Hash calls per second: " << num_items * num_hashes * num_trials / time_taken << std::endl;
 
+#ifdef CHECK_QUALITY
   std::cout << "Col depth: ";
   for (int i = 0; i < 32; i++) {
-    std::cout << (double)(zeros[i]) / (double)(num_items * col_width * num_trials) << ", ";
+    std::cout << (double)(zeros[i]) / (double)(num_items * num_hashes * num_trials) << ", ";
   }
   std::cout << std::endl;
+#endif
 }
 
-void run_64bit_vector_test(uint64_t num_items, uint64_t col_width, int num_trials) {
+void run_64bit_vector_test(uint64_t num_items, uint64_t num_hashes, int num_trials) {
   std::vector<long> zeros(32);
   long seed = 0xBAAABAAA;
   std::mt19937_64 rand(seed);
+  vec_hash_t hash_xor = 0;
 
   auto start = std::chrono::steady_clock::now();
   for (int t = 0; t < num_trials; t++) {
     for (uint64_t i = 0; i < num_items; i++) {
       vec_t index = rand();
-      std::vector<vec_hash_t> hashes = get_hash64_vector(index, seed, col_width);
+      std::vector<vec_hash_t> hashes = get_hash64_vector(index, seed, num_hashes);
       for (vec_hash_t hash : hashes) {
+	hash_xor ^= hash;
+#ifdef CHECK_QUALITY	
         int j = 1;
         while(hash % (1 << j) == 0 && j <= 32) j++;
-
         zeros[j-1]++;
+#endif
       }
     }
     
@@ -170,6 +197,7 @@ void run_64bit_vector_test(uint64_t num_items, uint64_t col_width, int num_trial
   auto end = std::chrono::steady_clock::now();
   long double time_taken = static_cast<std::chrono::duration<long double>>(end - start).count();
   std::cout << "======  64 bit hash vector ======" << std::endl;
+  std::cout << "hash xor " << hash_xor << std::endl;
 
   long sum = 0;
   for (int i = 31; i >= 0; i--) {
@@ -178,34 +206,40 @@ void run_64bit_vector_test(uint64_t num_items, uint64_t col_width, int num_trial
   }
 
   std::cout << "Average time: " << time_taken / num_trials << std::endl;
+  std::cout << "Hash calls per second: " << num_items * num_hashes * num_trials / time_taken << std::endl;
 
+#ifdef CHECK_QUALITY
   std::cout << "Col depth: ";
   for (int i = 0; i < 32; i++) {
-    std::cout << (double)(zeros[i]) / (double)(num_items * col_width * num_trials) << ", ";
+    std::cout << (double)(zeros[i]) / (double)(num_items * num_hashes * num_trials) << ", ";
   }
   std::cout << std::endl;
+#endif
 }
 
 int main(int argc, char** argv) {
   if (argc != 4) {
     std::cout << "Number of arguments is incorrect. 3 required" << std::endl;
-    std::cout << "num_items, col_width, num_trials" << std::endl;
+    std::cout << "Arguments: num_items, num_hashes, num_trials" << std::endl;
+    std::cout << "num_items:  The number of items to hash, randomly generated" << std::endl;
+    std::cout << "num_hashes: Hash the item with this many unique hash seeds" << std::endl;
+    std::cout << "num_trials: Repeat the experiment this many times" << std::endl;
     exit(1); 
   }
 
   uint64_t num_items = std::atoll(argv[1]);
-  uint64_t col_width = std::atoll(argv[2]);
+  uint64_t num_hashes = std::atoll(argv[2]);
   int num_trials = std::atoi(argv[3]);
 
   /*
    * 32 bit tests
    */
-  run_32bit_direct_test(num_items, col_width, num_trials);
-  run_32bit_vector_test(num_items, col_width, num_trials);
+  run_32bit_direct_test(num_items, num_hashes, num_trials);
+  run_32bit_vector_test(num_items, num_hashes, num_trials);
   
   /*
    * 64 bit tests
    */
-  run_64bit_direct_test(num_items, col_width, num_trials);  
-  run_64bit_vector_test(num_items, col_width, num_trials);
+  run_64bit_direct_test(num_items, num_hashes, num_trials);  
+  run_64bit_vector_test(num_items, num_hashes, num_trials);
 }
