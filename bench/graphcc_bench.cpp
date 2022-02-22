6ac3b128dbc80befbd878b99428faff6301cc0fb
Evan West <etwest@cs.stonybrook.edu>
2022-02-22 11:42:10 -0500

    fixed bug where benchmark would run inifinitely and increased minTime

--- bench/graphcc_bench.cpp
+++ bench/graphcc_bench.cpp
@@ -5,16 +5,18 @@ constexpr uint64_t KB = 1024;
 
 // Test the speed of reading all the data in the kron15 graph stream
 static void BM_FileIngest(benchmark::State &state) {
-	BinaryGraphStream stream("/mnt/ssd2/binary_streams/kron_15_stream_binary", state.range(0));
+	for (auto _ : state) {
+		BinaryGraphStream stream("/mnt/ssd2/binary_streams/kron_15_stream_binary", state.range(0));
 
-	uint64_t m = stream.edges();
-	GraphUpdate upd;
-	while (m--) {
-		benchmark::DoNotOptimize(upd = stream.get_edge());
+		uint64_t m = stream.edges();
+		GraphUpdate upd;
+		while (m--) {
+			benchmark::DoNotOptimize(upd = stream.get_edge());
+		}
 	}
 }
 
-BENCHMARK(BM_FileIngest)->Arg(KB)->Arg(32 * KB)->Arg(64 * KB);
+BENCHMARK(BM_FileIngest)->MinTime(15)->Arg(KB)->Arg(32 * KB)->Arg(64 * KB);
 
 
 BENCHMARK_MAIN();
