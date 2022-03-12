f83d1af68bb73231cd96a5a68d00e8def18cd511
Victor Zhang <vcz4@scarletmail.rutgers.edu>
2022-03-11 20:59:17 -0500

    feat: use xxh3

--- include/types.h
+++ include/types.h
@@ -3,8 +3,13 @@
 #include <graph_zeppelin_common.h>
 
 typedef uint64_t col_hash_t;
-static const auto& vec_hash = XXH32;
-static const auto& col_hash = XXH64;
+
+static unsigned int XXH3_32(const void* a, unsigned long b, unsigned
+int c) {
+  return (unsigned int) XXH3_64bits_withSeed(a,b,c);
+}
+static const auto& vec_hash = XXH3_32;
+static const auto& col_hash = XXH3_64bits_withSeed;
 
 enum UpdateType {
   INSERT = 0,
