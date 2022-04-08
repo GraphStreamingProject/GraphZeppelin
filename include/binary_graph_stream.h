ec3c280ab88f2c93bd45efe3619709a4e035dde7
Evan West <evtwest@yahoo.com>
2022-04-08 14:46:58 -0400

    query functionality

--- include/binary_graph_stream.h
+++ include/binary_graph_stream.h
@@ -57,7 +57,7 @@ private:
     buf = start_buf;
     bin_file.read(buf, buf_size);
   }
-  const uint32_t edge_size = sizeof(uint8_t) + 2 * sizeof(uint32_t); // size of a binary encoded edge
+  const uint32_t edge_size = sizeof(uint8_t) + 2 * sizeof(uint32_t); // size of binary encoded edge
   std::ifstream bin_file; // file to read from
   char *buf;              // data buffer
   char *start_buf;        // the start of the data buffer
@@ -76,6 +76,7 @@ public:
       throw BadStreamException();
     }
 
+    // set the buffer size to be a multiple of an edge size
     buf_size = _b - (_b % edge_size); // ensure buffer size is multiple of edge_size
 
     // read header from the input file
@@ -85,22 +86,53 @@ public:
       throw BadStreamException();
     end_of_file = (num_edges * edge_size) + 12;
     stream_off = 12;
+    query_block = false;
   }
+
+  /*
+   * Call this function to register a query in advance to avoid contraint on 32 KiB boundary
+   * Only one query may be registered at a time and query index must within a 32 KiB boundary not
+   * yet touched by the MT_StreamReader threads.
+   * To register first query, call before processing any updates from the stream
+   * When registering second (or later) query, call register_query before post_query_resume to 
+   * ensure the call succeeds
+   * @param query_idx   the stream update index directly after which the query will be performed
+   * @return            true if the query is successfully registered and false if not
+  */
+  bool register_query(uint64_t query_idx) {
+    uint64_t byte_index = 12 + query_idx * edge_size;
+    if (byte_index <= stream_off) return false;
+    else query_index = byte_index;
+    return true;
+  }
+
+  // Call this function to ask stream to pause so we can perform a query
+  // This allows queries to be performed in the stream arbitrary at 32 KiB granularity
+  // without advance notice
+  void on_demand_query() { query_block = true; }
+
+  // call this function to tell stream its okay to keep going
+  // call once per query performed regardless if registered query or on-demand query
+  void post_query_resume() { query_block = false; }
+
   inline uint32_t nodes() {return num_nodes;}
   inline uint64_t edges() {return num_edges;}
   BinaryGraphStream_MT(const BinaryGraphStream_MT &) = delete;
   BinaryGraphStream_MT & operator=(const BinaryGraphStream_MT &) = delete;
   friend class MT_StreamReader;
-
 private:
   int stream_fd;
   std::atomic<uint64_t> stream_off;
-  uint32_t num_nodes;     // number of nodes in the graph
-  uint64_t num_edges;     // number of edges in the graph stream
-  uint32_t buf_size;      // how big is the data buffer
-  const uint32_t edge_size = sizeof(uint8_t) + 2 * sizeof(uint32_t); // size of a binary encoded edge
-  uint64_t end_of_file;
+  uint32_t num_nodes;    // number of nodes in the graph
+  uint64_t num_edges;    // number of edges in the graph stream
+  uint32_t buf_size;     // how big is the data buffer
+  uint64_t end_of_file;  // the index of the end of the file
+  uint64_t query_index;  // what is the index of the next query in bytes
+  std::atomic<bool> query_block; // If true block read_data() calls and have thr return NXT_QUERY
+  const uint32_t edge_size = sizeof(uint8_t) + 2 * sizeof(uint32_t); // size of binary encoded edge
+
   inline uint32_t read_data(char *buf) {
+    if (query_block) return 0; // we are blocking on a query so don't fetch_add or read
     uint64_t read_off = stream_off.fetch_add(buf_size, std::memory_order_relaxed);
     if (read_off >= end_of_file) return 0;
     
@@ -117,17 +149,16 @@ private:
 // BinaryGraphStream_MT from a single thread
 class MT_StreamReader {
 public:
-  MT_StreamReader(BinaryGraphStream_MT &stream) : stream(stream) {
-    // set the buffer size to be a multiple of an edge size and malloc memory
-    buf = (char *) malloc(stream.buf_size * sizeof(char));
-    start_buf = buf;
-  }
+  MT_StreamReader(BinaryGraphStream_MT &stream) :
+    stream(stream), buf((char *) malloc(stream.buf_size * sizeof(char))), start_buf(buf) {}
 
   inline GraphUpdate get_edge() {
     // if we have read all the data in the buffer than refill it
     if (buf - start_buf == data_in_buf) {
       if ((data_in_buf = stream.read_data(start_buf)) == 0) {
-        return {{-1, -1}, END_OF_FILE};
+        if (stream.query_block)
+          return {{-1, -1}, NXT_QUERY}; // return that a query should be processed before continuing
+        return {{-1, -1}, END_OF_FILE}; // return that the stream is over
       }
       buf = start_buf; // point buf back to beginning of data buffer
     }
@@ -139,14 +170,14 @@ public:
     std::memcpy(&a, buf + 1, sizeof(uint32_t));
     std::memcpy(&b, buf + 5, sizeof(uint32_t));
     
-    buf += stream.edge_size; 
+    buf += stream.edge_size;
 
     return {{a,b}, u};
   }
 
 private:
-  char *buf;              // data buffer
-  char *start_buf;        // the start of the data buffer
-  uint32_t data_in_buf = 0;
-  BinaryGraphStream_MT &stream;
+  BinaryGraphStream_MT &stream; // stream to pull data from
+  char *buf;                    // data buffer
+  char *start_buf;              // the start of the data buffer
+  uint32_t data_in_buf = 0;     // amount of data in data buffer
 };
