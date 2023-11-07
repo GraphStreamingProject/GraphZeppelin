17647a4adfc4869a7891fb9fd92f9d199f381066
jhwjhw0123 <jhwjhw0123@gmail.com>
2023-11-07 15:56:45 -0500

    test sending edges to artificial stream

--- tools/process_stream.cpp
+++ tools/process_stream.cpp
@@ -56,56 +56,99 @@ void track_insertions(uint64_t total, GraphSketchDriver<CCSketchAlg> *driver,
 }
 
 int main(int argc, char **argv) {
-  if (argc != 4) {
-    std::cout << "ERROR: Incorrect number of arguments!" << std::endl;
-    std::cout << "Arguments: stream_file, graph_workers, reader_threads" << std::endl;
-    exit(EXIT_FAILURE);
-  }
+    if (argc != 4) {
+        std::cout << "ERROR: Incorrect number of arguments!" << std::endl;
+        std::cout << "Arguments: stream_file, graph_workers, reader_threads" << std::endl;
+        exit(EXIT_FAILURE);
+    }
 
-  shutdown = false;
-  std::string stream_file = argv[1];
-  int num_threads = std::atoi(argv[2]);
-  if (num_threads < 1) {
-    std::cout << "ERROR: Invalid number of graph workers! Must be > 0." << std::endl;
-    exit(EXIT_FAILURE);
-  }
-  size_t reader_threads = std::atol(argv[3]);
+    shutdown = false;
+    std::string stream_file = argv[1];
+    int num_threads = std::atoi(argv[2]);
+    if (num_threads < 1) {
+        std::cout << "ERROR: Invalid number of graph workers! Must be > 0." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    size_t reader_threads = std::atol(argv[3]);
 
-  BinaryFileStream stream(stream_file);
-  node_id_t num_nodes = stream.vertices();
-  size_t num_updates  = stream.edges();
-  std::cout << "Processing stream: " << stream_file << std::endl;
-  std::cout << "nodes       = " << num_nodes << std::endl;
-  std::cout << "num_updates = " << num_updates << std::endl;
-  std::cout << std::endl;
+    BinaryFileStream stream(stream_file);
+    BinaryFileStream stream2(stream_file);
+    node_id_t num_nodes = stream.vertices();
+    size_t num_updates  = stream.edges();
+    std::cout << "Processing stream: " << stream_file << std::endl;
+    std::cout << "nodes       = " << num_nodes << std::endl;
+    std::cout << "num_updates = " << num_updates << std::endl;
+    std::cout << std::endl;
 
-  auto driver_config = DriverConfiguration().gutter_sys(CACHETREE).worker_threads(num_threads);
-  auto cc_config = CCAlgConfiguration().batch_factor(1);
-  CCSketchAlg cc_alg{num_nodes, cc_config};
-  GraphSketchDriver<CCSketchAlg> driver{&cc_alg, &stream, driver_config, reader_threads};
+    auto driver_config = DriverConfiguration().gutter_sys(CACHETREE).worker_threads(num_threads);
+    auto cc_config = CCAlgConfiguration().batch_factor(1);
+    CCSketchAlg cc_alg{num_nodes, cc_config};
+    GraphSketchDriver<CCSketchAlg> driver{&cc_alg, &stream, driver_config, reader_threads};
 
-  auto ins_start = std::chrono::steady_clock::now();
-  std::thread querier(track_insertions, num_updates, &driver, ins_start);
+    auto driver_config2 = DriverConfiguration().gutter_sys(CACHETREE).worker_threads(num_threads);
+    auto cc_config2 = CCAlgConfiguration().batch_factor(1);
+    CCSketchAlg cc_alg2{num_nodes, cc_config2};
+    GraphSketchDriver<CCSketchAlg> driver2{&cc_alg2, &stream2, driver_config2, reader_threads};
 
-  driver.process_stream_until(END_OF_STREAM);
+    auto ins_start = std::chrono::steady_clock::now();
+    std::thread querier(track_insertions, num_updates, &driver, ins_start);
 
-  auto cc_start = std::chrono::steady_clock::now();
-  driver.prep_query();
-  auto CC_num = cc_alg.connected_components().size();
-  std::chrono::duration<double> insert_time = driver.flush_end - ins_start;
-  std::chrono::duration<double> cc_time = std::chrono::steady_clock::now() - cc_start;
-  std::chrono::duration<double> flush_time = driver.flush_end - driver.flush_start;
-  std::chrono::duration<double> cc_alg_time = cc_alg.cc_alg_end - cc_alg.cc_alg_start;
+    driver.process_stream_until(END_OF_STREAM);
+    driver2.process_stream_until(END_OF_STREAM);
 
-  shutdown = true;
-  querier.join();
+    auto cc_start = std::chrono::steady_clock::now();
+    driver.prep_query();
+    driver2.prep_query();
+    auto CC_num = cc_alg.connected_components().size();
 
-  double num_seconds = insert_time.count();
-  std::cout << "Total insertion time(sec):    " << num_seconds << std::endl;
-  std::cout << "Updates per second:           " << stream.edges() / num_seconds << std::endl;
-  std::cout << "Total CC query latency:       " << cc_time.count() << std::endl;
-  std::cout << "  Flush Gutters(sec):           " << flush_time.count() << std::endl;
-  std::cout << "  Boruvka's Algorithm(sec):     " << cc_alg_time.count() << std::endl;
-  std::cout << "Connected Components:         " << CC_num << std::endl;
-  std::cout << "Maximum Memory Usage(MiB):    " << get_max_mem_used() << std::endl;
+    std::vector<std::pair<node_id_t, std::vector<node_id_t>>> forest = cc_alg.calc_spanning_forest();
+
+    GraphUpdate temp_edge;
+
+    temp_edge.type = DELETE;
+
+    for(unsigned int j=0;j<forest.size();j++)
+    {
+        std::cout <<"Node "<< j <<": " << std::endl;
+        cc_alg2.apply_update_batch(0,forest[j].first,forest[j].second);
+        for(unsigned int i=0;i<forest[j].second.size();i++)
+        {
+            std::cout <<"Nbr "<< i <<": " << forest[j].second[i] << std::endl;
+            temp_edge.edge.src=forest[j].first;
+            temp_edge.edge.dst=forest[j].second[i];
+            cc_alg2.update(temp_edge);
+        }
+    }
+
+
+    std::vector<std::pair<node_id_t, std::vector<node_id_t>>> forest2 = cc_alg2.calc_spanning_forest();
+
+    for(unsigned int j=0;j<forest2.size();j++)
+    {
+        std::cout <<"Node "<< j <<": " << std::endl;
+        for(unsigned int i=0;i<forest2[j].second.size();i++)
+        {
+            std::cout <<"Nbr "<< i <<": " << forest2[j].second[i] << std::endl;
+        }
+    }
+
+    auto CC_num2 = cc_alg2.connected_components().size();
+
+    std::chrono::duration<double> insert_time = driver.flush_end - ins_start;
+    std::chrono::duration<double> cc_time = std::chrono::steady_clock::now() - cc_start;
+    std::chrono::duration<double> flush_time = driver.flush_end - driver.flush_start;
+    std::chrono::duration<double> cc_alg_time = cc_alg.cc_alg_end - cc_alg.cc_alg_start;
+
+    shutdown = true;
+    querier.join();
+
+    double num_seconds = insert_time.count();
+    std::cout << "Total insertion time(sec):    " << num_seconds << std::endl;
+    std::cout << "Updates per second:           " << stream.edges() / num_seconds << std::endl;
+    std::cout << "Total CC query latency:       " << cc_time.count() << std::endl;
+    std::cout << "  Flush Gutters(sec):           " << flush_time.count() << std::endl;
+    std::cout << "  Boruvka's Algorithm(sec):     " << cc_alg_time.count() << std::endl;
+    std::cout << "Connected Components:         " << CC_num << std::endl;
+    std::cout << "Connected Components2:         " << CC_num2 << std::endl;
+    std::cout << "Maximum Memory Usage(MiB):    " << get_max_mem_used() << std::endl;
 }
