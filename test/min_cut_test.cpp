b8072088278a3313e2a504d0d06383fa6eb0f72c
Evan West <evtwest@yahoo.com>
2025-07-14 15:51:28 -0400

    a few more changes

--- test/min_cut_test.cpp
+++ test/min_cut_test.cpp
@@ -17,6 +17,32 @@ static size_t get_seed() {
   return s;
 }
 
+// Test for constructing mincut and driver. Just to ensure everything work
 TEST(MinCutTest, Construction) {
+  const std::string fname = __FILE__;
+  size_t pos = fname.find_last_of("\\/");
+  const std::string curr_dir = (std::string::npos == pos) ? "" : fname.substr(0, pos);
+  AsciiFileStream stream{curr_dir + "/res/multiples_graph_1024.txt", false};
+
   MinCutSketchAlg mc_alg(1024, get_seed());
+  GraphSketchDriver<MinCutSketchAlg> driver(&mc_alg, &stream, DriverConfiguration());
 }
+
+TEST(MinCutTest, DisconnectedMinCut) {
+  auto driver_config = DriverConfiguration();
+  const std::string fname = __FILE__;
+  size_t pos = fname.find_last_of("\\/");
+  const std::string curr_dir = (std::string::npos == pos) ? "" : fname.substr(0, pos);
+  AsciiFileStream stream{curr_dir + "/res/multiples_graph_1024.txt", false};
+  node_id_t num_nodes = stream.vertices();
+
+  MinCutSketchAlg mc_alg(1024, get_seed());
+
+  GraphSketchDriver<CCSketchAlg> driver(&cc_alg, &stream, driver_config);
+  driver.process_stream_until(END_OF_STREAM);
+  driver.prep_query(MINIMUMCUT);
+  driver.check_verifier(GraphVerifier(1024, curr_dir + "/res/multiples_graph_1024.txt"));
+
+  ASSERT_EQ(0, mc_alg.minimum_cut().value);
+}
+
